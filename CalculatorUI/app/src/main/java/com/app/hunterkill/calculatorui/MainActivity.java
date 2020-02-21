9fbf37bdfde55c6709535c16a805a10344eba296
hunterkillzero <snguoila@yahoo.com>
2020-02-21 17:56:24 +0700

    done ex 4

--- CalculatorUI/app/src/main/java/com/app/hunterkill/calculatorui/MainActivity.java
+++ CalculatorUI/app/src/main/java/com/app/hunterkill/calculatorui/MainActivity.java
@@ -2,14 +2,28 @@ package com.app.hunterkill.calculatorui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     TextView txtResult;
-    Button btnAC, btnDel, btnMod, btnAdd, btnSub, btnMul, btnDiv, btnEqual;
-    Button btnNum0, btnNum1, btnNum2, btnNum3, btnNum4, btnNum5, btnNum6, btnNum7, btnNum8, btnNum9, btnDot;
+    int[] idUniButton = {
+            R.id.btnAC,
+            R.id.btnDel,
+            R.id.btnMod,
+            R.id.btnAdd,
+            R.id.btnSub,
+            R.id.btnMul,
+            R.id.btnDiv,
+            R.id.btnEqual,
+            R.id.btnDot
+    };
+//    Button btnAC, btnDel, btnMod, btnAdd, btnSub, btnMul, btnDiv, btnEqual, btnDot;
+
+    Button[] btnNum;
+//    Button btnNum0, btnNum1, btnNum2, btnNum3, btnNum4, btnNum5, btnNum6, btnNum7, btnNum8, btnNum9, btnDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,36 @@ public class MainActivity extends Activity {
 
         txtResult = findViewById(R.id.Result);
 
+        for (int i = 0; i < idUniButton.length; i++) {
+            Button b = (Button) findViewById(idUniButton[i]);
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // do something
+                    if (v.getId() == R.id.btnAC) {
+                        txtResult.setText("0");
+                    }
+                }
+            });
+        }
 
+        btnNum = new Button[10];
+        for (int i = 0; i < btnNum.length; i++) {
+            String buttonId = "btnNum" + i;
+            int resId = getResources().getIdentifier(buttonId, "id", getPackageName());
+            btnNum[i] = findViewById(resId);
+            final int finalI = i;
+            btnNum[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // do something
+
+                    if (finalI == 1) {
+                        txtResult.setText(finalI + "");
+                    }
+                }
+            });
+        }
     }
+
 }
