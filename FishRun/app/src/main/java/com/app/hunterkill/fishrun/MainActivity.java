c32810e6993e57669dd84802b6a880eb1b349939
hunterkillzero <snguoila@yahoo.com>
2020-04-13 17:24:25 +0700

    add score, fish motion, heart life

--- FishRun/app/src/main/java/com/app/hunterkill/fishrun/MainActivity.java
+++ FishRun/app/src/main/java/com/app/hunterkill/fishrun/MainActivity.java
@@ -1,13 +1,37 @@
 package com.app.hunterkill.fishrun;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    private FlyingFishView gameView;
+
+    private Handler handler = new Handler();
+    private final static long interval = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        gameView = new FlyingFishView(this);
+        setContentView(gameView);
+
+        Timer time = new Timer();
+        time.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        gameView.invalidate();
+
+                    }
+                });
+            }
+        }, 0, interval);
     }
 }
