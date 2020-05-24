812d238eade889b008d4df6c31839fa389b9df86
hunterkillzero <snguoila@yahoo.com>
2020-05-24 09:38:08 +0700

    restore current checkbox when rotating; bug crashing app when orotation

--- NameBakin/app/src/main/java/com/app/hunterkill/namebakin/IngredientFragment.java
+++ NameBakin/app/src/main/java/com/app/hunterkill/namebakin/IngredientFragment.java
@@ -18,6 +18,9 @@ import android.widget.Toast;
 
 public class IngredientFragment extends Fragment {
 
+    private static final String KEY_CHECK_BOXES = "key_check_boxes" ;
+    private CheckBox[] mCheckBoxes;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,20 +31,43 @@ public class IngredientFragment extends Fragment {
         LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.ingredientLayout);
         String [] ingredients = Recipes.ingredients[index].split("`");
 
-        setUpCheckBox(ingredients, linearLayout);
+        mCheckBoxes = new CheckBox[ingredients.length];
+        boolean[] checkBoxes = new boolean[mCheckBoxes.length];
+        if (savedInstanceState != null && savedInstanceState.getBooleanArray(KEY_CHECK_BOXES) != null)
+            checkBoxes = savedInstanceState.getBooleanArray(KEY_CHECK_BOXES);
+
+
+        setUpCheckBox(ingredients, linearLayout, checkBoxes);
 
         return view;
     }
 
-    private void setUpCheckBox(String [] ingredients, ViewGroup container) {
+    private void setUpCheckBox(String[] ingredients, ViewGroup container, boolean[] checkBoxes) {
+        int i = 0;
+
         for(String ingredient: ingredients) {
-            CheckBox checkBox = new CheckBox(getActivity());
-            checkBox.setPadding(8, 16, 8, 16);
-            checkBox.setTextSize(20f);
-            checkBox.setText(ingredient);
-            checkBox.setTextColor(Color.RED);
-            System.out.println(ingredient);
-            container.addView(checkBox);// add to linearLayout
+            mCheckBoxes[i] = new CheckBox(getActivity());
+            mCheckBoxes[i].setPadding(8, 16, 8, 16);
+            mCheckBoxes[i].setTextSize(20f);
+            mCheckBoxes[i].setText(ingredient);
+            mCheckBoxes[i].setTextColor(Color.RED);
+            container.addView( mCheckBoxes[i]);// add to linearLayout
+            if (checkBoxes[i]) {
+                mCheckBoxes[i].toggle();
+            }
+            i++;
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        boolean[] stateOfCheckBoxes = new boolean[mCheckBoxes.length];
+        int i = 0;
+        for (CheckBox checkBox: mCheckBoxes) {
+            stateOfCheckBoxes[i] = checkBox.isChecked();
+            i++;
+        }
+        outState.putBooleanArray(KEY_CHECK_BOXES, stateOfCheckBoxes);
+        super.onSaveInstanceState(outState);
     }
 }
